62e048129be367ed8545c54d679f90632977cb98
Lim Sungjun <juny9610@naver.com>
2021-07-08 14:34:29 +0900

    2206 벽 부수고 이동하기

--- BOJ/BFS, DFS, Backtracking/2206_벽부수고이동하기.cpp
+++ BOJ/BFS, DFS, Backtracking/2206_벽부수고이동하기.cpp
@@ -1,54 +1,51 @@
 #include <iostream>
+#include <string>
 #include <queue>
-#include <stdio.h>
 
 using namespace std;
 #define MAX 1001
 
 int N, M;
-int arr[MAX][MAX];
-int visit[MAX][MAX][2];
-int step = 0;
+int map[MAX][MAX];
+bool visit[MAX][MAX][2];
 
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
 
-int bfs(int x, int y) {
-    queue< pair< pair<int,int>, int> > q;
-    visit[x][y][0] = 1;
-    int w = 0;
-    q.push(make_pair(make_pair(x,y),w));
-    
+int bfs() {
+    queue< pair< pair<int,int>, pair<int,int> > > q;
+    q.push(make_pair(make_pair(0,0),make_pair(1,0)));
+    visit[0][0][0] = true;
+
     while(!q.empty()) {
-        x = q.front().first.first;
-        y = q.front().first.second;
-        w = q.front().second;
+        int x = q.front().first.first;
+        int y = q.front().first.second;
+        int cnt = q.front().second.first;
+        int w = q.front().second.second;
         q.pop();
 
-        if(x == N-1 && y == M-1) return visit[x][y][w];
+        if(x == N-1 && y == M-1) return cnt;
 
         for(int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            int nw = w;
 
             if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
             if(visit[nx][ny][w]) continue;
 
-            if(arr[nx][ny] == 0) {
-                visit[nx][ny][nw] = visit[x][y][w] + 1;
-                q.push(make_pair(make_pair(nx,ny),w));
+            if(map[nx][ny] == 0) {
+                visit[nx][ny][w] = true;
+                q.push(make_pair(make_pair(nx,ny), make_pair(cnt+1,w)));
             }
-
-            if(arr[nx][ny] == 1 && nw == 0) {
-                visit[nx][ny][1] = visit[x][y][w] + 1;
-                nw = 1;
-                q.push(make_pair(make_pair(nx,ny),nw));
+            else if(map[nx][ny] == 1) {
+                if(w == 0){
+                    visit[nx][ny][w+1] = true;
+                    q.push(make_pair(make_pair(nx,ny), make_pair(cnt+1,w+1)));
+                }
             }
         }
     }
     return -1;
-
 }
 
 int main() {
@@ -57,11 +54,11 @@ int main() {
         string tmp;
         cin >> tmp;
         for(int j = 0; j < M; j++) {
-            arr[i][j] = tmp[j] - '0';
+            map[i][j] = tmp[j] - '0';
         }
     }
 
-    cout << bfs(0,0) << endl;
+    cout << bfs() << endl;
 
     return 0;
 }
