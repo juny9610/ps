a135c6dc4187ccfdb1cf244a2b4dda53bc18aedd
Lim Sungjun <juny9610@naver.com>
2021-07-30 05:42:53 +0900

    1717 집합의 표현

---
+++ BOJ/트리, MST, Union find/1717_집합의표현.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+#define endl '\n'
+#define MAX 1000001
+
+int n, m;
+int parent[MAX];
+
+int find(int x) {
+    if(parent[x] == x) return x;
+    else return parent[x] = find(parent[x]);
+}
+
+bool merge(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if(x == y) return false;
+    if(x > y) parent[x] = y;
+    else parent[y] = x;
+    return true;
+}
+
+
+int main() {
+    ios_base :: sync_with_stdio(false); cin.tie(NULL);
+
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) parent[i] = i;
+    while(m--) {
+        int c, a, b;
+        cin >> c >> a >> b;
+        if(c == 0) merge(a,b);
+        else if(c == 1) {
+            if(find(a) == find(b)) cout << "YES" << endl;
+            else cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
